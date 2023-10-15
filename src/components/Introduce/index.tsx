d8c0cc8cdc7ae7cd60f83f0431e7b42b3d6a5e4c
n ghtcjm <84369321+choi138@users.noreply.github.com>
2023-10-15 18:58:53 +0900

    refactor: add section component

--- src/components/Introduce/index.tsx
+++ src/components/Introduce/index.tsx
@@ -6,6 +6,8 @@ import { SchoolLogoPng } from 'src/assets';
 import { ABOUT_ME_LIST, PROJECT_LIST } from 'src/constant';
 import { LiStyle, LinkStyle, UlStyle } from 'src/styles';
 
+import { Section } from '../common';
+
 import * as S from './styled';
 
 export interface IntroduceItems {
@@ -101,40 +103,13 @@ export const Introduce: React.FC = () => {
                 {title}
               </Text>
               <UlStyle key={title}>
-                <LiStyle>
-                  <TextContainer alignItems="center" columnGap={1}>
-                    <Text size={1.5} weight={400} color="#424242">
-                      {subTitle}
-                    </Text>
-                    <Text size={0.9} weight={400} color="#616161">
-                      {date}
-                    </Text>
-                  </TextContainer>
-                  <TextContainer flexDirection="column" rowGap={0.4}>
-                    {link ? (
-                      <>
-                        <Text size={0.9} color="#616161" weight={300} lineHeight={1.4}>
-                          {description}
-                          <br />
-                          <LinkStyle href={link} target="_blank" rel="noreferrer">
-                            {link}
-                          </LinkStyle>
-                        </Text>
-                      </>
-                    ) : (
-                      <Text size={0.9} color="#616161" weight={300}>
-                        {description}
-                      </Text>
-                    )}
-                    <TextContainer columnGap={0.4}>
-                      {tag.map((item) => (
-                        <Text size={0.8} color="#9e9e9e" weight={400} key={item}>
-                          #{item}
-                        </Text>
-                      ))}
-                    </TextContainer>
-                  </TextContainer>
-                </LiStyle>
+                <Section
+                  subTitle={subTitle}
+                  date={date}
+                  description={description}
+                  tag={tag}
+                  link={link}
+                />
               </UlStyle>
             </>
           ))}
--- src/components/common/Section/index.tsx
+++ src/components/common/Section/index.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
 
-import { LiStyle, UlStyle } from 'src/styles';
+import { Text, TextContainer } from '@choi138/react-text';
 
-export const Section: React.FC = () => {
+import { LiStyle, LinkStyle } from 'src/styles';
+
+export interface SectionProps {
+  subTitle: string;
+  date: string;
+  description: string;
+  link?: string;
+  tag: string[];
+}
+
+export const Section: React.FC<SectionProps> = ({ subTitle, date, link, description, tag }) => {
   return (
-    <UlStyle>
-      <LiStyle>1</LiStyle>
-    </UlStyle>
+    <LiStyle>
+      <TextContainer alignItems="center" columnGap={1}>
+        <Text size={1.5} weight={400} color="#424242">
+          {subTitle}
+        </Text>
+        <Text size={0.9} weight={400} color="#616161">
+          {date}
+        </Text>
+      </TextContainer>
+      <TextContainer flexDirection="column" rowGap={0.4}>
+        {link ? (
+          <>
+            <Text size={0.9} color="#616161" weight={300} lineHeight={1.4}>
+              {description}
+              <br />
+              <LinkStyle href={link} target="_blank" rel="noreferrer">
+                {link}
+              </LinkStyle>
+            </Text>
+          </>
+        ) : (
+          <Text size={0.9} color="#616161" weight={300}>
+            {description}
+          </Text>
+        )}
+        <TextContainer columnGap={0.4}>
+          {tag.map((item) => (
+            <Text size={0.8} color="#9e9e9e" weight={400} key={item}>
+              #{item}
+            </Text>
+          ))}
+        </TextContainer>
+      </TextContainer>
+    </LiStyle>
   );
 };
