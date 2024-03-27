c5ff13fa7ecddf5110f13bc3c2232376fa74596e
최근원 <kidjustinchoi@gmail.com>
2024-03-27 10:10:56 +0900

    Revert "chore: pnpm run format and lint"
    This reverts commit 56ff7964583561fcc107900332a48d30749e8204.

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -20,7 +20,7 @@ export const MainPage: React.FC = () => {
   };
 
   const handlePrevClick = () => {
-    if (mainPageRef.current) {
+    if (mainPageRef.current && page > 1) {
       setPage(page - 1);
       mainPageRef.current.scrollLeft -= window.innerWidth;
     }
@@ -40,7 +40,7 @@ export const MainPage: React.FC = () => {
       <Study />
       <Awards />
       <Project />
-      <S.NavbarContainer opacity={page <= 1 ? 0 : 1}>
+      <S.NavbarContainer opacity={page >= 1 ? 1 : 0}>
         <BiLeftArrowAlt size={30} onClick={handlePrevClick} />
         <S.NavbarRightIcon size={30} onClick={handleNextClick} opacity={page > 4 ? 0 : 1} />
       </S.NavbarContainer>
